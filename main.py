68da55bdf9d8cba274a8d29145aa7a0c0da0addd
Paudel-A <122073307+Paudel-A@users.noreply.github.com>
2023-01-06 01:19:58 -0800

    Update main.py
    just presented a small alternative if you need to just print the name of the food and its value side by side.

--- main.py
+++ main.py
@@ -18,25 +18,40 @@ food_prices = {
 print(food_prices)            
 
 # Retrieve any value from the food prices dictionary by using its corresponding key along with the bracket notation
-# For example, to get the price for cheese, run this:
+# For example, to get the price for milk, run this:
 milk_price = food_prices["milk"]
 print(milk_price)
 
 # Access the prices for each item & store into their own variable, just like the milk example.  
 chicken_price = food_prices["chicken"]
 print(chicken_price)
+
 beef_price = food_prices["beef"]
 print(beef_price)
+
 cheese_price = food_prices["cheese"]
 print(cheese_price)
-# Print those variables to check that you get the right value. Write the code to print on the line below:
+
 egg_price = food_prices["egg"]
 print(egg_price)
+
 carrot_price = food_prices["carrot"]
 print(carrot_price)
+
 tomato_price = food_prices["tomato"]
 print(tomato_price)
+
 cauliflower_price = food_prices["cauliflower"]
+print(cauliflower_price)
+
+# Even though the task in hand asks you to create a variable to store the values of prices within the variable 
+# If you just need to print the key values pair there is a code that can be used in two lines 
+for food, price in food_prices.items():
+  print(f"{food} : {price}") # these values are stored as string 
+  
+# If you need to just print the prices of food the following code can also be applied 
+for price in food_prices.values():
+  print(f"{price}")
 
 ## Part II. Create a list of the Spotify playlist
 
@@ -50,12 +65,13 @@ playlist = [
 
 # Print the playlist in the line below:
 print(playlist)
-first_song = playlist[0]        
+
+first_song = playlist[0] # playlist[-4] also works       
+print(first_song)
+
 # Lists have an index, which is a number assigned to each item in the list based on the oder in which they appear in the list, starting at 0. 
 # What's the first song on your list?
 #Oliver Tree - Life goes on
-
-print(first_song)
 
 # Pick your top 2 favorite songs from the list & print them using the list's index to access them
 fave_songs = playlist[1:3]  
@@ -78,9 +94,9 @@ print(playlist)
 
 # How many songs are in your list? Use len()
 print(len(playlist))
-# Write here how many songs are in your playlist = 
+# Write here how many songs are in your playlist = 4
 
 # Sort your playlist in alphabetical order using sorted()
 print(sorted(playlist))
 # Write here the order of the songs in your playlist:
-# 12345
+# ['Conan Gray - Maniac', 'J cole - Middle Child', 'Oliver Tree - Life Goes On', 'Post Malone - Wow']
